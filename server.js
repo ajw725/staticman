a8c811b26ae88c04f374408136c03b4731b0aa02
Andrew Weinstein <andrew@altvia.com>
2020-12-05 23:32:44 -0700

    email customization

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ class StaticmanAPI {
 
   initialiseCORS () {
     this.server.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || '*')
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
       next()
